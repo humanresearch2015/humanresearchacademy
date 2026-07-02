ef43ccf7fbd6411e6f18da4782f170b908a81635
Saimado <humanresearch2025@gmail.com>
2026-07-02 17:02:46 +0200

    fix forgot password page

---
+++ app/auth/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <h1>Recuperar Palavra-passe</h1>
+
+      <input placeholder="Digite o seu email" />
+
+      <button>Enviar link de recuperação</button>
+    </div>
+  );
+}
