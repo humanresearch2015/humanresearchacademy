32864141885f6754b135d04ac1f6a4aba748ef51
Saimado <humanresearch2025@gmail.com>
2026-07-03 14:33:34 +0200

    Projeto mínimo funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>HOME OK</h1>;
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Human Research Academy</h1>
+
+      <ul>
+        <li><Link href="/login">Login</Link></li>
+        <li><Link href="/cursos">Cursos</Link></li>
+        <li><Link href="/cursos-gratuitos">Cursos Gratuitos</Link></li>
+        <li><Link href="/cursos-pagos">Cursos Pagos</Link></li>
+      </ul>
+    </main>
+  );
 }
