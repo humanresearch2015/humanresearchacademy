ed9ffb239141979d1e4fa863b621b2a0f6417e05
Saimado <humanresearch2025@gmail.com>
2026-07-03 15:43:14 +0200

    add lista de aulas no curso

--- app/cursos-pagos/page.tsx
+++ app/cursos-pagos/page.tsx
@@ -1,11 +1,43 @@
 import Link from "next/link";
 
-export default function CursosPagos() {
+export default function PesquisaCientifica() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Cursos Pagos</h1>
+    <main style={container}>
+      <h1>📚 Pesquisa Científica Completa</h1>
 
-      <Link href="/cursos">Voltar</Link>
+      <p style={text}>
+        Curso completo para desenvolvimento de projetos de pesquisa científica.
+      </p>
+
+      <h2>📖 Aulas</h2>
+
+      <ul style={list}>
+        <li>
+          <Link href="/cursos-pagos/pesquisa-cientifica/aulas/introducao">
+            📌 Aula 1: Introdução à Pesquisa
+          </Link>
+        </li>
+      </ul>
+
+      <div style={{ marginTop: 30 }}>
+        <Link href="/cursos-pagos">← Voltar</Link>
+      </div>
     </main>
   );
 }
+
+const container = {
+  padding: 40,
+  fontFamily: "Arial",
+};
+
+const text = {
+  color: "#475569",
+  maxWidth: 700,
+  lineHeight: 1.6,
+};
+
+const list = {
+  marginTop: 20,
+  lineHeight: 2,
+};
